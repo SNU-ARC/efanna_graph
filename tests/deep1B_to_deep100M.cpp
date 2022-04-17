ffaae40f8eb579541b135f2df7bd3134ef08a521
k16diablo <miguel92@naver.com>
2022-04-17 23:42:15 +0900

    Avoid stack smashing

--- tests/deep1B_to_deep100M.cpp
+++ tests/deep1B_to_deep100M.cpp
@@ -14,36 +14,36 @@ void load_data(char* filename, float*& data, unsigned& num,unsigned& dim){// loa
   std::ios::pos_type ss = in.tellg();
   size_t fsize = (size_t)ss;
   num = (unsigned)(fsize / (dim+1) / 4);
-  num = 100000000;
-  data = new float[num * dim * sizeof(float)];
-
-  in.seekg(0,std::ios::beg);
-  for(size_t i = 0; i < num; i++){
-    in.seekg(4,std::ios::cur);
-    in.read((char*)(data+i*dim),dim*4);
-  }
 
   // SJ: num of DEEP100M sub dataset is 6.25M
   unsigned int sub_num = num / 16;
-  for (unsigned int iter = 0; iter < sub_num; iter++) {
-    char iter_char[2];
+  data = new float[sub_num * dim * sizeof(float)];
+  in.seekg(0,std::ios::beg);
+
+  for (unsigned int iter = 0; iter < 16; iter++) {
+    for(size_t i = 0; i < sub_num; i++){
+      in.seekg(4 ,std::ios::cur);
+      in.read((char*)(data + i * dim), dim * 4);
+      if (i % 1000000 == 0)
+        std::cout << i << std::endl;
+    }
+
+    char iter_char[3];
     std::sprintf(iter_char, "%d", iter);
-    char* sub_filename = new char[strlen("deep100M_base.fvecs") + 3];
-    strcat(sub_filename, "deep100M_base_");
+    char* sub_filename = new char[strlen("deep100M_base.fvecs") + 4];
+    strcpy(sub_filename, "deep100M_base_");
     strcat(sub_filename, iter_char);
     strcat(sub_filename, ".fvecs");
+    std::cout << sub_filename << std::endl;
     
     std::ofstream out(sub_filename, std::ios::binary | std::ios::out);
     out.write((char*)&sub_num, sizeof(unsigned int));
-    for(size_t i = 0; i < num; i++){
-      out.write((char*)(data + iter * sub_num + i * dim), dim * 4);
+    for(size_t i = 0; i < sub_num; i++){
+      out.write((char*)(data + i * dim), dim * 4);
     }
     out.close();
   }
-//  std::ofstream out("./deep100M_base.fvecs", std::ios::binary | std::ios::out);
-//  out.write((char*)&num, sizeof(unsigned int));
   in.close();
-//  out.close();
 }
 int main(int argc, char** argv){
   if(argc!=2){std::cout<< argv[0] <<" data_file"<<std::endl; exit(-1);}
