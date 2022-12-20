2f27149be7348650c462ac33c3d310e81feed38a
k16diablo <miguel92@naver.com>
2022-12-20 22:27:35 +0900

    description

--- tests/build_graph.sh
+++ tests/build_graph.sh
@@ -25,18 +25,6 @@ efanna_gist1M() {
   fi
 }
 
-efanna_glove-100() {
-  if [ ! -f "glove-100_400nn.graph" ]; then
-    if [ ! -d "glove-100" ]; then
-      echo "ERROR: glove-100 dataset does not exist"
-      exit 1
-    else
-      echo "Building glove-100_400nn.graph KNN graph"
-      ./test_nndescent glove-100/glove-100_base.fvecs glove-100_400nn.graph 400 420 12 20 200
-    fi
-  fi
-}
-
 efanna_crawl() {
   if [ ! -f "crawl_400nn.graph" ]; then
     if [ ! -d "crawl" ]; then
@@ -84,17 +72,14 @@ elif [ "${1}" == "deep1M" ]; then
   efanna_deep1M
 elif [ "${1}" == "deep100M" ]; then
   efanna_deep100M
-elif [ "${1}" == "glove-100" ]; then
-  efanna_glove-100
 elif [ "${1}" == "crawl" ]; then
   efanna_crawl
 elif [ "${1}" == "all" ]; then
   efanna_sift1M
   efanna_gist1M
-  efanna_deep1M
-#  efanna_deep100M
-  efanna_glove-100
   efanna_crawl
+  efanna_deep1M
+  efanna_deep100M
 else
-  echo "Please use either 'sift1M' or 'gist1M' or 'deep1M' or 'glove-100' or 'crawl' or 'all' as an argument"
+  echo "Usage: ./build_graph.sh [dataset]"
 fi
